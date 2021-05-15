3112745a9408490c72be3e908fc10c25c160a469
echcherrate.ismail <echcherrate.ismail@gmail.com>
2021-05-15 15:13:34 +0000

    serving public folder as static and /public path with ./public folder.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 absolutePath = __dirname + "/views/index.html";
-console.log(absolutePath);
+
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
     // res.send('Hello Express');
     res.sendFile(absolutePath);
