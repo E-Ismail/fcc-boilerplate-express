b93400cb50a28715065530e742d16e4301fb5788
echcherrate.ismail <echcherrate.ismail@gmail.com>
2021-05-15 15:03:36 +0000

    serve static files using sendFile().

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+absolutePath = __dirname + "/views/index.html";
+console.log(absolutePath);
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    // res.send('Hello Express');
+    res.sendFile(absolutePath);
 });
 
 
